ece2cafc4ac4f1a80c70d1fa01acdb08f39292c2
HANYONG LEE <glhy0718@gmail.com>
2020-11-10 04:44:39 +0900

    init

---
+++ app/src/main/java/com/example/rec_commend/ui/option/OptionFragment.java
@@ -0,0 +1,26 @@
+package com.example.rec_commend.ui.option;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.rec_commend.R;
+import com.example.rec_commend.ui.home.HomeViewModel;
+
+public class OptionFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_option, container, false);
+        return root;
+    }
+}
