3ef01704717c1f17ac13a43a6ceb3dfa6911e09e
HANYONG LEE <glhy0718@gmail.com>
2020-11-14 03:14:59 +0900

    wave, loding animation

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Screen shots
 
-screenshot1 | screenshot2 | screenshot3 | screenshot4 | screenshot5
---|--|--|--|--
-![Screenshot_Home](./home.png) | ![Screenshot_Music_Search](./Music_Search.png) | ![Screenshot_Tune_Search](./Tune_Search.png) | ![Screenshot_Result](./Result.png) | ![Screenshot_Option](./Option.png)
+
+![](./screenshot/home.png) | ![](./screenshot/Music_Search.png) | ![](./screenshot/Tune_Search.png) | ![](./screenshot/Result.png)
+--|--|--|--
+![](./screenshot/search1.png)|![](./screenshot/search2.png)|![](./screenshot/Option.png)|
--- app/src/main/java/com/example/rec_commend/ui/SearchFragment.java
+++ app/src/main/java/com/example/rec_commend/ui/SearchFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -45,7 +46,14 @@ public abstract class SearchFragment extends Fragment {
     private ImageView recOverlay;
     private Button jsonTestBtn;
     private TextView timerText;
+    private WaveFormView waveFormView;
+    private ProgressBar progressBar;
 
+    //abstract functions
+    abstract protected void setDescription();
+    abstract protected void setPostURL();
+
+    //variable for recording
     private boolean isRecording;
     final private static String RECORDED_FILE_NAME = "record";
     private String MP4FilePath;
@@ -67,25 +75,35 @@ public abstract class SearchFragment extends Fragment {
     protected static String urlString;
     private static final String testJson = "[{\"id\":\"tmFmFNn5-OM\",\"title\":\"Puss (Prod.by \\ub77c\\uc774\\uba38)\",\"singer\":\"\\uc9c0\\ubbfc (AOA), \\uc544\\uc774\\uc5b8\",\"genre\":\"\\ub7a9\\/\\ud799\\ud569\",\"release\":\"2015.03.20\",\"timbre_similarity\":98.7300118817},{\"id\":\"LmApDbvNCXg\",\"title\":\"Outro : Ego\",\"singer\":\"\\ubc29\\ud0c4\\uc18c\\ub144\\ub2e8\",\"genre\":\"\\ub7a9\\/\\ud799\\ud569\",\"release\":\"2020.02.21\",\"timbre_similarity\":139.5603603933},{\"id\":\"dj-OEG32OR4\",\"title\":\"Forever (Prod. By GRAY)\",\"singer\":\"BewhY (\\ube44\\uc640\\uc774)\",\"genre\":\"\\ub7a9\\/\\ud799\\ud569\",\"release\":\"2016.07.02\",\"timbre_similarity\":140.948202667},{\"id\":\"E-HXf-Dih3c\",\"title\":\"\\uc0ac\\uc774\\uba3c \\ub3c4\\ubbf8\\ub2c9\",\"singer\":\"\\uc0ac\\uc774\\uba3c \\ub3c4\\ubbf8\\ub2c9\",\"genre\":\"\\ub7a9\\/\\ud799\\ud569\",\"release\":\"2015.08.21\",\"timbre_similarity\":143.5430228883},{\"id\":\"aFsS59DkFhc\",\"title\":\"Okey Dokey\",\"singer\":\"MINO (\\uc1a1\\ubbfc\\ud638), \\uc9c0\\ucf54 (ZICO)\",\"genre\":\"\\ub7a9\\/\\ud799\\ud569\",\"release\":\"2015.08.29\",\"timbre_similarity\":153.3020229043},{\"id\":\"vRXZj0DzXIA\",\"title\":\"Ice Cream (with Selena Gomez)\",\"singer\":\"BLACKPINK\",\"genre\":\"\\ub304\\uc2a4\",\"release\":\"2020.08.28\",\"timbre_similarity\":161.693719075},{\"id\":\"VTASffPQGhY\",\"title\":\"\\ub204\\ub09c \\ub108\\ubb34 \\uc608\\ubed0 (Replay)\",\"singer\":\"SHINee (\\uc0e4\\uc774\\ub2c8)\",\"genre\":\"\\ub304\\uc2a4\",\"release\":\"2008.05.22\",\"timbre_similarity\":162.2474205873},{\"id\":\"of2GzuZGxo0\",\"title\":\"STILL ALIVE\",\"singer\":\"BIGBANG\",\"genre\":\"\\ub304\\uc2a4\",\"release\":\"2012.06.03\",\"timbre_similarity\":166.8970531035},{\"id\":\"jTYtNlSnn7g\",\"title\":\"I` ll Be There\",\"singer\":\"\\uc774\\ube0c\",\"genre\":\"\\ub85d\\/\\uba54\\ud0c8\",\"release\":\"2001.04.11\",\"timbre_similarity\":168.3398402292},{\"id\":\"wd2qbIh6wYQ\",\"title\":\"Moon\",\"singer\":\"\\ubc29\\ud0c4\\uc18c\\ub144\\ub2e8\",\"genre\":\"\\ub7a9\\/\\ud799\\ud569\",\"release\":\"2020.02.21\",\"timbre_similarity\":170.050908522}]\n";
 
-    abstract protected void setDescription();
-    abstract protected void setPostURL();
+    //wave form view thread
+    private Thread waveThread;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //setting UI elements
         View root = inflater.inflate(R.layout.fragment_search, container, false);
-        recBtn = root.findViewById(R.id.rec_btn);
-        recOverlay = root.findViewById(R.id.rec_overlay);
-        jsonTestBtn = root.findViewById(R.id.json_test_btn);
-        timerText = root.findViewById(R.id.timer);
+
         descriptionText = root.findViewById(R.id.search_description);
         setDescription();
 
+        recBtn = root.findViewById(R.id.rec_btn);
         recBtn.setImageResource(R.drawable.ic_baseline_fiber_manual_record_24);
+
+        recOverlay = root.findViewById(R.id.rec_overlay);
         recOverlay.setVisibility(View.INVISIBLE);
-        timerViewInit();
 
+        timerText = root.findViewById(R.id.timer);
+        initTimerView();
+
+        waveFormView = root.findViewById(R.id.siriView);
+        initWaveFormView();
+
+        jsonTestBtn = root.findViewById(R.id.json_test_btn);
+
+        progressBar = root.findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        //recording setting
         ActivityCompat.requestPermissions(getActivity(), permissions, REQUEST_RECORD_AUDIO_PERMISSION);
-
         String commonPath = Paths.get(getContext().getDataDir().getPath(), RECORDED_FILE_NAME).toString();
         MP4FilePath = commonPath + ".mp4";
         MP3FilePath = commonPath + ".mp3";
@@ -94,17 +112,11 @@ public abstract class SearchFragment extends Fragment {
         isRecording = false;
         recBtn.setOnClickListener((view)->{
             if(!isRecording){
-                recBtn.setImageResource(R.drawable.ic_baseline_stop_24);
-                recOverlay.setVisibility(View.VISIBLE);
                 isRecording = true;
                 startRecording(MP4FilePath);
-                timerViewStart();
             }else{
-                recBtn.setImageResource(R.drawable.ic_baseline_fiber_manual_record_24);
-                recOverlay.setVisibility(View.INVISIBLE);
                 isRecording = false;
                 stopRecording(view);
-                timerViewInit();
             }
         });
 
@@ -125,6 +137,7 @@ public abstract class SearchFragment extends Fragment {
         }
         if(postThread != null) postThread.interrupt();
         if(ti != null) ti.cancel();
+        if(waveThread != null) waveThread.interrupt();
     }
 
     @Override
@@ -140,6 +153,11 @@ public abstract class SearchFragment extends Fragment {
     }
 
     private void startRecording(String path) {
+        recBtn.setImageResource(R.drawable.ic_baseline_stop_24);
+        recOverlay.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+        timerViewStart();
+
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
@@ -152,15 +170,23 @@ public abstract class SearchFragment extends Fragment {
             Log.e(LOG_TAG, "prepare() failed : " + e.getMessage());
         }
         recorder.start();
+
+        activateWaveFormView();
     }
 
     private void stopRecording(View view) {
+        recBtn.setVisibility(View.INVISIBLE);
+        recOverlay.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+        initTimerView();
+        timerText.setText("잠시만 기다리세요...");
+        initWaveFormView();
+
         recorder.stop();
         recorder.reset();
         recorder.release();
         recorder = null;
         convertMP4ToMP3AndPost(view, MP4FilePath, MP3FilePath);
-        timerViewInit();
     }
 
     private void convertMP4ToMP3AndPost(View view, String inputFile, String outputFile){
@@ -231,8 +257,33 @@ public abstract class SearchFragment extends Fragment {
         timer.schedule(ti,0,1000);
     }
 
-    private void timerViewInit(){
+    private void initTimerView(){
         if(ti != null) ti.cancel();
         timerText.setText("");
+    }
+
+    private void activateWaveFormView(){
+        if(waveThread != null) waveThread.interrupt();
+        waveThread = null;
+        waveThread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    waveFormView.updateSpeaking(true);
+                    while (recorder!=null){
+                        int amp = recorder.getMaxAmplitude();
+                        waveFormView.updateAmplitude(amp);
+                        System.out.println(amp);
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        waveThread.start();
+    }
+    private void initWaveFormView(){
+        if(waveThread != null) waveThread.interrupt();
+        waveFormView.updateSpeaking(false);
     }
 }
